621b778ec697c07a5d8358c9c50080230f5061ea
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2015-03-08 19:32:48 -0300

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meetinglean::Application.config.secret_key_base = '8ee582ba21744ea021641896581b24bd120bb8cf3feab3e5e9a4c082b39b424b2bba12a336e9fd8015f054fcc628c7daae103e759b4891aa3f52d51ade9969bc'
