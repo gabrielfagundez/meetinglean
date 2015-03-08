8336c7630f0562c9632566ac6144b2da6a5033e0
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2015-03-08 20:28:43 -0300

    Add initial assets for landing page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,10 @@
 //= require_tree ./angular/services
 //= require_tree ./angular/controllers
 //= require_tree ./angular/templates
+
+// Landing
+//= require agency/agency
+//= require agency/cbpAnimatedHeader
+//= require agency/classie
+//= require agency/contact_me
+//= require agency/jqBootstrapValidation
