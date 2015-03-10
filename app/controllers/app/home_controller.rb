405064f0359c183294a166403824899089c10fc5
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2015-03-10 19:21:22 -0300

    Add new landing styles

---
+++ app/controllers/app/home_controller.rb
@@ -0,0 +1,6 @@
+class App::HomeController < ApplicationController
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Meetinglean::Application.routes.draw do
 
   root to: 'landing#index'
 
+  namespace :app do
+    root to: 'home#index'
+  end
+
 end
