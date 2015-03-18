46a418eabcdac798f4c160efb008122a09e14990
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2015-03-18 19:01:46 -0300

    Add update method for meetings

--- app/controllers/api/meetings_controller.rb
+++ app/controllers/api/meetings_controller.rb
@@ -5,18 +5,44 @@ class Api::MeetingsController < ApplicationController
   end
 
   def show
-    meeting = Meeting.includes(:meeting_agendas).find(params[:id])
-    render json: {
-      id: meeting.id,
-      name: meeting.name,
-      description: meeting.description,
-      agenda: meeting.meeting_agendas,
-      private_notes: meeting.private_notes,
-      meeting_decisions: meeting.meeting_decisions,
-      meeting_open_issues: meeting.meeting_open_issues,
-      meeting_action_items: meeting.meeting_action_items,
-      summary: meeting.meeting_summaries
-    }
+    meeting = Meeting.find_by_id(params[:id])
+    if meeting.present?
+      render json: meeting.full_json_data
+    else
+      render json: {}
+    end
+  end
+
+  def update
+    meeting = Meeting.find_by_id(params[:id])
+    if meeting
+      meeting.name = params[:name] if params[:name].present?
+      meeting.description = params[:description] if params[:description].present?
+      update_related_item(meeting, params[:agenda], MeetingAgenda) if params[:agenda].present?
+      update_related_item(meeting, params[:private_notes], PrivateNote) if params[:private_notes].present?
+      update_related_item(meeting, params[:meeting_decisions], MeetingDecision) if params[:meeting_decisions].present?
+      update_related_item(meeting, params[:meeting_open_issues], MeetingOpenIssue) if params[:meeting_open_issues].present?
+      update_related_item(meeting, params[:meeting_action_items], MeetingActionItem) if params[:meeting_action_items].present?
+      update_related_item(meeting, params[:summary], MeetingSummary) if params[:summary].present?
+
+      render json: meeting.full_json_data
+    else
+      render json: {}
+    end
+  end
+
+  private
+
+  def update_related_item(meeting, items, klass)
+    items.each do |item|
+      if item[:id].present?
+        meeting_agenda = klass.find(item[:id])
+        meeting_agenda.description = item[:description]
+        meeting_agenda.save
+      else
+        MeetingAgenda.create(meeting_id: meeting.id, description: meeting_agenda[:description])
+      end
+    end
   end
 
 end
