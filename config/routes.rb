73033ce2089ceaa74871791785920d8d76127228
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2015-03-18 19:01:19 -0300

    Enable REST routes for meetings

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Meetinglean::Application.routes.draw do
   root to: 'landing#index'
 
   namespace :api do
-    resources :meetings, only: [:index, :show]
+    resources :meetings
   end
 
   namespace :app do
