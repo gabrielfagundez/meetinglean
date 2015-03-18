593844811425544bf5fc2798e0d58b0aef880867
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2015-03-18 20:04:34 -0300

    Return components ordered by created at asc

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -12,12 +12,12 @@ class Meeting < ActiveRecord::Base
         id: self.id,
         name: self.name,
         description: self.description,
-        agenda: self.meeting_agendas.order('created_at DESC').all,
-        private_notes: self.meeting_private_notes.order('created_at DESC'),
-        meeting_decisions: self.meeting_decisions.order('created_at DESC'),
-        meeting_open_issues: self.meeting_open_issues.order('created_at DESC'),
-        meeting_action_items: self.meeting_action_items.order('created_at DESC'),
-        summary: self.meeting_summaries.order('created_at DESC')
+        meeting_agendas: self.meeting_agendas.order('created_at ASC').all,
+        meeting_private_notes: self.meeting_private_notes.order('created_at ASC'),
+        meeting_decisions: self.meeting_decisions.order('created_at ASC'),
+        meeting_open_issues: self.meeting_open_issues.order('created_at ASC'),
+        meeting_action_items: self.meeting_action_items.order('created_at ASC'),
+        meeting_summaries: self.meeting_summaries.order('created_at ASC')
     }.to_json
   end
 
