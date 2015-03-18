f31bcb66209dc98bf16a685704724a6e6515da5d
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2015-03-18 19:00:52 -0300

    Add json method in meeting model

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -7,4 +7,18 @@ class Meeting < ActiveRecord::Base
   has_many :meeting_open_issues
   has_many :meeting_action_items
 
+  def full_json_data
+    {
+        id: self.id,
+        name: self.name,
+        description: self.description,
+        agenda: self.meeting_agendas,
+        private_notes: self.private_notes,
+        meeting_decisions: self.meeting_decisions,
+        meeting_open_issues: self.meeting_open_issues,
+        meeting_action_items: self.meeting_action_items,
+        summary: self.meeting_summaries
+    }.to_json
+  end
+
 end
