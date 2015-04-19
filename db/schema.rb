d621dfa54e3b5ef79561b7d6d1765d643a8b6552
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2015-04-19 18:21:41 -0300

    Add user id to meetings table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150329220829) do
+ActiveRecord::Schema.define(version: 20150408001514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20150329220829) do
     t.datetime "end_time"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
